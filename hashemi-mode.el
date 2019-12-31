629cd30e30480f514614048eeb0ae62ae74e1e06
Vahid Naeini <naeini.v@gmail.com>
2019-12-31 10:09:50 +0330

    Update hashemi-mode.el

--- hashemi-mode.el
+++ hashemi-mode.el
@@ -1,3 +1,6 @@
+(defun hashemi-mode-re-grab (inner) (concat "\\(" inner "\\)"))
+(defun hashemi-mode-re-word (inner) (concat "\\<" inner "\\>"))
+
 (defconst hashemi-mode-syntax-table
   (let ((table (make-syntax-table)))
     (modify-syntax-entry ?' "\"" table)
@@ -10,26 +13,34 @@
   :syntax-table hashemi-mode-syntax-table
   (font-lock-fontify-buffer))
 
+(defface hashem-mode-builtin-face
+  '((t :inherit font-lock-builtin-face))
+  "Face for builtins."
+  :group 'hashemi-mode)
+
+(defvar hashemi-mode-builtin
+  '("bekhoon" "bechap" "benevis" "jadid")
+  "List of builtins")
 
 (defconst hashemi-mode-keywords
-  '("bebin"
-    "badi" "khob"
-    "age" "bood"
-    "na?" "ta"
-    "bede" 
-    "yield"))
+  '("bebin" "badi" "khob" "age" "bood" "na?" "ta" "bede"))
 
-(font-lock-add-keywords 'hashemi-mode
-			'(( "\\(bebin\\)[ \t\n]+\\(\\(\\sw\\|\\s_\\)+\\)"
-			    (2 'font-lock-variable-name-face)
-			    (1 'font-lock-keyword-face)
-			    )))
 
-(font-lock-add-keywords 'hashemi-mode
-			'(( "\\(ta\\)[ \t\n].*[ \t\n]+\\(bood\\)"
-			    (1 'font-lock-keyword-face)
-			    (2 'font-lock-keyword-face)
-			    )))
+(font-lock-add-keywords 'hashemi-mode '(( "\\(bebin\\|badi\\|khob\\|ta\\|age\\|na\\?\\|bede\\)"
+					  (1 'font-lock-keyword-face))))
+
+
+(font-lock-add-keywords 'hashemi-mode '(( "\\(bebin\\)[ \t\n]+\\(\\(\\sw\\|\\s_\\)+\\)"
+					  (2 'font-lock-warning-face)
+					  (1 'font-lock-keyword-face))))
+
+(font-lock-add-keywords 'hashemi-mode '(( "\\(age\\|ta\\)[ \t\n].*[ \t\n]+\\(bood\\)"
+					  (1 'font-lock-keyword-face)
+					  (2 'font-lock-keyword-face))))
+
+(font-lock-add-keywords 'hashemi-mode  '(( "[ \t\n]\\(\\(\\sw\\|\\s_\\)+\\)[ \t\n]*=[\t\n]*.*\\(;\\)"
+					   (1 'font-lock-type-face)
+					   (2 'font-lock-negation-face))))
 
 (add-to-list 'auto-mode-alist '("\\.hashem\\'" . hashemi-mode))
 
